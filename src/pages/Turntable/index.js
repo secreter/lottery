68e0c4a4c4e62b7941055c6e06116021813ed995
So <1979510177@qq.com>
2019-05-07 23:40:42 +0800

    draw general turntable

--- src/pages/Turntable/index.js
+++ src/pages/Turntable/index.js
@@ -12,10 +12,30 @@ class Turntable extends React.Component {
       status:this.STATUS.STOP
     }
 
-    this.TOTAL=9
+    this.TOTAL=10
+
+    this.radius=375/2
     this.degree=360/this.TOTAL
+    this.halfDegree=this.degree/2
     this.skewDeg=90-this.degree
+    this.textRadius=0.5*this.radius //文字的上边到圆心的距离
+    this.textBlockHeight=24 //文字块高度
     this.sectors=new Array(this.TOTAL).fill(Object.create({}))
+  }
+  getTurntableItemStyle=(index)=>{
+    return {
+      //居然和顺序有关
+      transform: `translate(100%,100%) rotate(${-(this.halfDegree+90)+this.degree*index}deg) skew(${this.skewDeg}deg)`
+    }
+  }
+  getTurntableItemTextStyle=(index)=>{
+    let y=this.textRadius
+    let x=(this.textRadius-this.textBlockHeight)*Math.tan(this.halfDegree*Math.PI/180)
+    return {
+      width:2*x+'px',
+      //居然和顺序有关
+      transform: `skew(${-this.skewDeg}deg) rotate(${(this.halfDegree+90)}deg) translate(${-x}px,${-y}px)`
+    }
   }
   componentDidMount () {
     this.init()
@@ -47,20 +67,20 @@ class Turntable extends React.Component {
 
               let style={
                 //居然和顺序有关
-                transform: `translate(50%,50%) rotate(${this.degree*index}deg) skew(${this.skewDeg}deg)`
+                // transfororm: `translate(50%,50%) rotate(${this.degree*index}deg) skew(${this.skewDeg}deg)`
               }
               let styleInner={
-                transform:`translate(50%,50%) rotate(${this.degree*0}deg) skew(-0deg)`
+                // transform:`translate(50%,50%) rotate(${this.degree*0}deg) skew(-0deg)`
               }
               let imgBoxStyle={
-                transform:`skew(-${this.skewDeg}deg) rotate(${this.degree/2+90}deg) translate(-${Math.tan(this.degree/2*Math.PI/180)*70}px,-100px)`
+                // transform:`skew(-${this.skewDeg}deg) rotate(${this.degree/2+90}deg) translate(-${Math.tan(this.degree/2*Math.PI/180)*70}px,-100px)`
               }
               return (
                 <Fragment key={index}>
                   <div
-                    style={style}
-                    className="turntable-item-bg">
-                      <div className="img-box" style={imgBoxStyle}>
+                    style={this.getTurntableItemStyle(index)}
+                    className="turntable-item">
+                      <div className="turntable-item-text" style={this.getTurntableItemTextStyle(index)}>
                         好
                       </div>
                   </div>
