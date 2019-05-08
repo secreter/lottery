49158b73c9f8279efc956ac769ae2f482c602921
So <1979510177@qq.com>
2019-05-09 01:23:12 +0800

    rotate to target bonus

--- src/pages/Turntable/index.js
+++ src/pages/Turntable/index.js
@@ -10,10 +10,29 @@ class Turntable extends React.Component {
     }
     this.state = {
       status:this.STATUS.STOP,
-      rotateDegree:0
+      rotateDegree:0   //记录已经旋转的度数
     }
 
-    this.TOTAL=10
+    this.bonus=[{
+      name:'机',
+    },{
+      name:'话费',
+    },{
+      name:'电影票'
+    },{
+      name:'谢谢参与'
+    },{
+      name:'手机',
+    },{
+      name:'话费',
+    },{
+      name:'电影票'
+    },{
+      name:'谢谢参与'
+    }]
+    // this.bonus=this.bonus.reverse() //因为盘是顺时针转的，
+    this.targetBonus='手机'
+    this.TOTAL=this.bonus.length||4
 
     this.radius=375/2
     this.degree=360/this.TOTAL
@@ -24,7 +43,23 @@ class Turntable extends React.Component {
     this.sectors=new Array(this.TOTAL).fill(true).map(_=>({})) //{dom,rotateDegree}
     this.init()
   }
-
+  get randomDegree(){
+    //(-a/2,a/2)
+    return this.getRandom(-this.halfDegree+3,this.halfDegree-3)
+  }
+  get nextDegree(){
+    //下次转动到的度数
+    const {rotateDegree}=this.state
+    const targetSectorIndex=this.bonus.findIndex((item)=>item.name===this.targetBonus)
+    const singleTargetDegree=this.degree*targetSectorIndex //单圈的目标度数中点，
+    const lastStopSingleDegree=rotateDegree%360
+    const nextStopSingleDegree=singleTargetDegree-lastStopSingleDegree  //要转到目标扇区需要的度数
+    console.log(rotateDegree,singleTargetDegree,lastStopSingleDegree,nextStopSingleDegree)
+    return rotateDegree+this.getRandom(8,10)*360+nextStopSingleDegree+this.randomDegree
+  }
+  getRandom=(from,to)=>{
+    return Math.floor(Math.random()*(to-from))+from
+  }
   getTurntableStyle=()=>{
     return {
       //居然和顺序有关
@@ -33,8 +68,8 @@ class Turntable extends React.Component {
   }
   getTurntableItemStyle=(index)=>{
     return {
-      //居然和顺序有关
-      transform: `translate(100%,100%) rotate(${-(this.halfDegree+90)+this.degree*index}deg) skew(${this.skewDeg}deg)`
+      //居然和顺序有关  // 逆时针绘制，因为转盘顺时针转动
+      transform: `translate(100%,100%) rotate(${-(this.halfDegree+90)-this.degree*index}deg) skew(${this.skewDeg}deg)`
     }
   }
   getTurntableItemTextStyle=(index)=>{
@@ -64,9 +99,12 @@ class Turntable extends React.Component {
     this.sectors[index]={dom:ref}
   }
   start=()=>{
-    this.rotate()
+
+    console.log(this.nextDegree)
+    this.rotate(this.nextDegree)
     this.setState({
-      status:this.STATUS.ROTATTING
+      status:this.STATUS.ROTATTING,
+      rotateDegree:this.nextDegree
     })
   }
   render(){
@@ -87,7 +125,7 @@ class Turntable extends React.Component {
                       style={this.getTurntableItemStyle(index)}
                       className="turntable-item">
                       <div className="turntable-item-text" style={this.getTurntableItemTextStyle(index)}>
-                        好
+                        {this.bonus[index].name}
                       </div>
                     </div>
                   </Fragment>
